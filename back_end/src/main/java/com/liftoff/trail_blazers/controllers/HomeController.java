aeebf2c9b5c4ffe514157278fb192d08aa88785a
Braden Evans <bradene0@outlook.com>
2025-12-12 09:55:10 -0600

    Seed database from bundled CSV files

--- back_end/src/main/java/com/liftoff/trail_blazers/controllers/HomeController.java
+++ back_end/src/main/java/com/liftoff/trail_blazers/controllers/HomeController.java
@@ -1,5 +1,14 @@
 package com.liftoff.trail_blazers.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
+    @GetMapping({"/", "/health"})
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("TrailBlazers API is running");
+    }
 }
